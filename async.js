2fda739bf22fde2326427df7f5f1202f989ebdee
Samuel <samuelrivba@gmail.com>
2024-04-11 17:17:02 -0500

    Add Parallel, Sequence and Race

--- async.js
+++ async.js
@@ -7,3 +7,33 @@ Promise.resolve("Hi").then((data) => console.log("3", data));
 
 // 3
 console.log("4", "is a crowd");
+
+const promisify = (item, delay) =>
+  new Promise((resolve) => setTimeout(() => resolve(item), delay));
+
+const a = () => promisify("a", 100);
+const b = () => promisify("b", 5000);
+const c = () => promisify("c", 3000);
+
+async function parallel() {
+  const promises = [a(), b(), c()];
+  const [output1, output2, output3] = await Promise.all(promises);
+  return `Parallel is done: ${output1} ${output2} ${output3}`;
+}
+
+async function race() {
+  const promises = [a(), b(), c()];
+  const output1 = await Promise.race(promises);
+  return `Race is done: ${output1}`;
+}
+
+async function sequence() {
+  const output1 = await a();
+  const output2 = await b();
+  const output3 = await c();
+  return `Sequence is done ${output1} ${output2} ${output3}`;
+}
+
+sequence().then(console.log);
+parallel().then(console.log);
+race().then(console.log);
